038e918bc0e513d7b4030cd081fea2e8fb5c02dc
Aadesh Shrestha <aadeshere1@gmail.com>
2019-06-23 20:39:50 +0545

    scrape each item from categories page and add pagination to scraping list

---
+++ parsers/categories.rb
@@ -0,0 +1,39 @@
+nokogiri = Nokogiri::HTML(content)
+
+searchlist = nokogiri.xpath('//div[@id="searchlist"]//div[@class="address-bar"]')
+
+PAGE_URL = page['vars']['cat_url']
+
+searchlist.each do |item|
+	url = item.xpath('./parent::a/@href').text
+	list_title = item.xpath('.//div[@class="business-name"]').text
+
+
+	pages << {
+		url: url,
+		page_type: "reviews",
+		fetch_type: "browser",
+		force_fetch:true,
+		vars: {
+			cat_title: page['vars']['cat_title'],
+			cat_url: page['vars']['cat_url']
+		}
+	}
+end
+
+paginations = nokogiri.xpath('//div[contains(@class, "page-of-pages")]').text.split
+start_page = paginations[1].to_i
+end_page = paginations[3].to_i
+
+(start_page..end_page).each do |page|
+	url = "#{PAGE_URL}?page=#{page}"
+
+	pages << {
+		url: url,
+		page_type: "categories",
+		vars: {
+			cat_title: page['vars']['cat_title'],
+			cat_url: page['vars']['cat_url']	
+		}
+	}
+end
